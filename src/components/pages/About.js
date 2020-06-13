2a21037daf6a52a7457f27642e10e002696fb128
Rhema Makamba <rhemaexel7@gmail.com>
2020-06-13 16:34:07 +0200

    src file

---
+++ src/components/pages/About.js
@@ -0,0 +1,13 @@
+import React, {Fragment} from 'react'
+
+const About = () => {
+  return (
+    <Fragment>
+          <h1>About Gihub-Finder</h1>
+          <p>If you are looking for a place to find github user you have come to the right place .This is the place to find ever one you are looking for</p>
+          <p>Version: 1.0.0</p>
+    </Fragment>
+  )
+}
+
+export default About
